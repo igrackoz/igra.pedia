14bd96612b7f15f04b64d63b6a0742e945e150d6
igrackoz <hectoraop@outlook.com>
2022-12-19 06:41:39 -0600

    formato de pacemaker hecho

--- src/app/components/escritura/pacemaker/pacemaker.component.ts
+++ src/app/components/escritura/pacemaker/pacemaker.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {trigger, style, animate, transition, state } from '@angular/animations';
 
 @Component({
   selector: 'app-pacemaker',
   templateUrl: './pacemaker.component.html',
-  styleUrls: ['./pacemaker.component.css']
+  styleUrls: ['./pacemaker.component.css'],
+  animations: [
+    trigger('fadeIn', [
+
+      // the "out" style determines the "resting" state of the element when it is visible.
+      state('out', style({opacity: 0})),
+      // in state
+      state('in', style({opacity: 1})),
+
+      transition('out => in', animate('1000ms ease-in')),
+    ])
+  ]
 })
 export class PacemakerComponent implements OnInit {
 
+  state="out";
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    
   }
 
 }
