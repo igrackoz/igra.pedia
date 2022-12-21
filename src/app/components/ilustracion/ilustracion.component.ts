8f11b88bbac1899c03dfe3b746f0fcbbb9fd5828
igrackoz <hectoraop@outlook.com>
2022-12-20 20:42:27 -0600

    transition image appearance

--- src/app/components/ilustracion/ilustracion.component.ts
+++ src/app/components/ilustracion/ilustracion.component.ts
@@ -1,12 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Storage, ref, uploadBytes, listAll, getDownloadURL } from '@angular/fire/storage';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import {trigger, style, animate, transition, state } from '@angular/animations';
 
 
 @Component({
   selector: 'app-ilustracion',
   templateUrl: './ilustracion.component.html',
-  styleUrls: ['./ilustracion.component.css']
+  styleUrls: ['./ilustracion.component.css'],
+  animations: [
+    trigger('appear', [
+
+      state('no', style({
+        opacity: '0',
+      })),
+      state('si', style({
+        opacity: '1',
+      })),
+      transition('closed => open', [
+        animate('0.5s cubic-bezier(.16,.67,.21,.97)')
+      ]),
+      transition('open => closed', [
+        animate('0.5s cubic-bezier(.16,.67,.21,.97)')
+      ]),
+    ])
+  ]
+
 })
 export class IlustracionComponent implements OnInit {
 
@@ -20,6 +39,12 @@ export class IlustracionComponent implements OnInit {
   ngOnInit(): void {
 
     this.getImages();
+  }
+
+  public aparicion = true;
+
+  toggle() {
+    this.aparicion = !this.aparicion;
   }
 
   getImages(){
